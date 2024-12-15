b03ff08f7a2a3849d38cbe6b70b961427346cbaa
varunmali <varumali234@gmail.com>
2024-12-15 22:05:56 +0530

    Learned about variables in JavaScript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553; //not changeable
+
+let accountEmail = "varun@google.com";
+
+var accountPassword = "12345";
+
+accountCity = "Jaipur";
+
+// accountId = 2; // not allowed
+
+accountEmail = "abc@avc.com";
+
+accountPassword = "456789";
+
+accountCity = "Gandhinagar";
+
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
