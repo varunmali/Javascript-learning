8183d0733d5881180eee0593f74530d890c36bdf
varunmali <varumali234@gmail.com>
2024-12-16 13:00:26 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // we are using nodejs, not browser
+
+console.log(3 + 3); // code readability should be high
+
+let name = "varun";
+let age = 18;
+let isLoggedIn = false;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => uniqueness
+// object
+
+console.log(typeof undefined);
+
+//typeof null is object.
