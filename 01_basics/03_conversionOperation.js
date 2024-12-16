c6bc7846bfae9bb369cd63fce3d53807e968d846
varunmali <varumali234@gmail.com>
2024-12-16 14:51:23 +0530

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33abc";
+
+console.log(typeof score);
+console.log(typeof score);
+
+let valuInNumber = Number(score);
+
+console.log(typeof valuInNumber);
+console.log(valuInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0;
+
+let isLoggenIn = 1;
+
+let booleanIsLoggenIn = Boolean(isLoggenIn);
+
+console.log(booleanIsLoggenIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "varun" => true
+
+let someNumber = 55;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
