bae0144bde51f0dabb5c510fea8511ee490dc47c
varunmali <varumali234@gmail.com>
2024-12-16 12:37:04 +0530

    add some comments

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,8 @@
 const accountId = 144553; //not changeable
 
-let accountEmail = "varun@google.com";
+let accountEmail = "varun@google.com"; // changeable
 
-var accountPassword = "12345";
+var accountPassword = "12345"; // changeable (N.P.)
 
 accountCity = "Jaipur";
 
@@ -15,4 +15,9 @@ accountPassword = "456789";
 accountCity = "Gandhinagar";
 
 // console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
 console.table([accountId, accountEmail, accountPassword, accountCity]);
