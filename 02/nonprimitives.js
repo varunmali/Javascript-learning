5638b8c405654270dc5632c1b41d59fdb7e0c150
varunmali <varumali234@gmail.com>
2024-12-20 18:56:49 +0530

    nonprimitives

---
+++ 02/nonprimitives.js
@@ -0,0 +1,24 @@
+const username = {
+  firstname: "varun",
+  isLoggedin: true,
+};
+
+// username.firstname = "Mr.V";
+username.lastname = "V";
+
+// console.log(username);
+// console.log(typeof username);
+
+// console.log(username.firstname);
+// console.log(username["isLoggedin"]);
+
+let today = new Date();
+// console.log(today.getDate())
+
+// Array
+
+// let heros = ["a","b","c" ]
+
+let anotherUser = ["varun", true];
+console.log(anotherUser);
+console.log(anotherUser[0]);
