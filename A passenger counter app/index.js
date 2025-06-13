674e02df5ba257a965ee4980c1fed68065b240a7
varunmali <varumali234@gmail.com>
2025-06-13 20:37:39 +0530

    People Entry Counter

---
+++ A passenger counter app/index.js
@@ -0,0 +1,10 @@
+let count = 0;
+
+let countEl = document.getElementById("count-el");
+console.log(countEl);
+
+function increment() {
+  console.log("clicked");
+  count = count + 1;
+  countEl.innerText = count;
+}
