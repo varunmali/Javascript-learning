cd26791fb8407bfc2f31668de944ca8da5b1f9c4
varunmali <varumali234@gmail.com>
2025-06-16 16:08:30 +0530

    Save data

--- A passenger counter app/index.js
+++ A passenger counter app/index.js
@@ -1,14 +1,16 @@
 let count = 0;
+let saveEl = document.getElementById("save-el");
 
 let countEl = document.getElementById("count-el");
 console.log(countEl);
 
 function increment() {
   console.log("clicked");
-  count = count + 1;
+  count += 1;
   countEl.innerText = count;
 }
 
 function save() {
-  console.log(count);
+  let countDash = count + "-";
+  saveEl.innerText += countDash;
 }
